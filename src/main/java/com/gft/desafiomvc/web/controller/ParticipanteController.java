fb911f9015713e299cebe21c636b67e1ad48cf3b
adao <aldo.santana@gft.com>
2022-10-25 22:17:03 -0300

    Tudo certo com participantes

--- src/main/java/com/gft/desafiomvc/web/controller/ParticipanteController.java
+++ src/main/java/com/gft/desafiomvc/web/controller/ParticipanteController.java
@@ -1,18 +1,30 @@
 package com.gft.desafiomvc.web.controller;
 
 import com.gft.desafiomvc.models.Participante;
+import com.gft.desafiomvc.service.ParticipanteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/participantes")
 public class ParticipanteController {
 
+    @Autowired
+    private ParticipanteService participanteService;
 
-    @GetMapping("/cadastrar")
-    public String cadastrar(Participante participante){
-        return "/participante/cadastro";
+    @RequestMapping(value = "/cadastrarParticipante", method = RequestMethod.GET)
+    public String form() {
+        return "/participante/formParticipante";
+    }
+
+
+    @RequestMapping(value = "cadastrarParticipante", method = RequestMethod.POST)
+    public String form(Participante participante) {
+        participanteService.saveParticipante(participante);
+        return "redirect:/cadastrarParticipantes";
+
     }
 
 
