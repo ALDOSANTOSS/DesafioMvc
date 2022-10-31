6672d78c9fcb2d07b80862f2222f3dd35a38ad21
adao <aldo.santana@gft.com>
2022-10-30 22:37:50 -0300

    Fazendo usuário

--- src/main/java/com/gft/desafiomvc/web/controller/AtividadeController.java
+++ src/main/java/com/gft/desafiomvc/web/controller/AtividadeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("atividade")
@@ -37,6 +39,21 @@ public class AtividadeController {
         mv.addObject("lista",atividadeService.bucarTodos());
         return mv;
 
+    }
+
+    @RequestMapping("/excluir")
+    public ModelAndView excluirAtividade(@RequestParam Long id, RedirectAttributes redirectAttributes) {
+
+        ModelAndView mv = new ModelAndView("redirect:/atividade");
+
+        try {
+            atividadeService.deleteAtividade(id);
+            redirectAttributes.addFlashAttribute("mensagem", "Atividade excluído com sucesso.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("mensagem", "Erro ao excluir Atividade!" + e.getMessage());
+        }
+
+        return mv;
     }
 
 
--- src/main/java/com/gft/desafiomvc/web/controller/UsuarioController.java
+++ src/main/java/com/gft/desafiomvc/web/controller/UsuarioController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("usuario")
@@ -17,18 +19,40 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @RequestMapping(path = "usuario")
+    @RequestMapping(path = "novo")
     public ModelAndView novoUsuario() {
         ModelAndView mv = new ModelAndView("usuario/novo.html");
         mv.addObject("usuario", new Usuario());
         return mv;
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "usuario")
+    @RequestMapping(method = RequestMethod.POST, path = "novo")
     public ModelAndView salvarUsuario(Usuario usuario) {
         ModelAndView mv = new ModelAndView("usuario/novo.html");
         mv.addObject("usuario", new Usuario());
         usuarioService.saveUsuario(usuario);
+        return mv;
+    }
+    @RequestMapping
+    public ModelAndView listarUsuario(){
+        ModelAndView mv = new ModelAndView("usuario/listar.html");
+        mv.addObject("lista",usuarioService.listaUsuario());
+        return mv;
+
+    }
+
+    @RequestMapping("/excluir")
+    public ModelAndView excluirEvento(@RequestParam Long id, RedirectAttributes redirectAttributes) {
+
+        ModelAndView mv = new ModelAndView("redirect:/usuario");
+
+        try {
+            usuarioService.deleteUsuario(id);
+            redirectAttributes.addFlashAttribute("mensagem", "Usuário excluído com sucesso.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("mensagem", "Erro ao excluir Usuario!" + e.getMessage());
+        }
+
         return mv;
     }
 
